f0f1a0307df9473b693ad4e7ab058de9b5d61246
Br4ndong <brandonmbraley@gmail.com>
2015-05-30 13:21:28 -0400

    Created a card json datatype
    Created a json card datatype to read in json data and populate the card
    data

---
+++ faraday/Card.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Text;
+using faraday.Data_Types;
+
+namespace faraday
+{
+    public partial class Card : PhoneApplicationPage
+    {
+        private CardJSON card = null;
+
+        public Card()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialize(String json)
+        {
+            card = new CardJSON(json);
+        }
+
+        public void Initialize(CardJSON card)
+        {
+            this.card = card;
+        }
+
+        public override string ToString()
+        {
+            if (card == null)
+                return "";
+
+            return "\nTitle:  " + card.Title +
+                   "\nBODY:  " + card.Body +
+                   "\nHELP:  " + card.Help +
+                   "\nTYPE:  " + card.Type +
+                   "\nID:  <" + card.ID + ">";
+        }
+    }
+}
---
+++ faraday/Data Types/JsonCard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faraday.Data_Types
+{
+    [DataContract]
+    public sealed class CardJSON
+    {
+        [DataMember(Name = "type", IsRequired = false)]
+        public string Type { get; set; }
+
+        [DataMember(Name = "title", IsRequired = false)]
+        public string Title { get; set; }
+
+        [DataMember(Name = "help", IsRequired = false)]
+        public string Help { get; set; }
+
+        [DataMember(Name = "body", IsRequired = false)]
+        public string Body { get; set; }
+
+        [DataMember(Name = "id", IsRequired = false)]
+        public string ID { get; set; }
+
+        public CardJSON()
+        {
+        }
+
+        public CardJSON(string json)
+        {
+            DataContractJsonSerializer parser = new DataContractJsonSerializer(typeof(string));
+
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+
+            Object tempCard = parser.ReadObject(stream);
+            if (tempCard is CardJSON)
+            {
+                CardJSON card = tempCard as CardJSON;
+
+                Type = card.Type;
+                Title = card.Title;
+                Help = card.Help;
+                Body = card.Body;
+                ID = card.ID;
+            }
+        }
+    }
+}
